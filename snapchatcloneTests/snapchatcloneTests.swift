6e60b16e1822b812bcab5994ba8443b06d0fc6c2
John Fantell <john@Johns-MacBook-Pro.local>
2017-07-07 10:35:06 -0400

    Initial Commit

---
+++ snapchatcloneTests/snapchatcloneTests.swift
@@ -0,0 +1,36 @@
+//
+//  snapchatcloneTests.swift
+//  snapchatcloneTests
+//
+//  Created by John Fantell on 7/7/17.
+//  Copyright © 2017 John Fantell. All rights reserved.
+//
+
+import XCTest
+@testable import snapchatclone
+
+class snapchatcloneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
